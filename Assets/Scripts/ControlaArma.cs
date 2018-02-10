279346a0206985c6c659e1f8108d90d41496436a
Pedro <pedro.lulio@hotmail.com>
2018-02-10 08:05:36 -0200

    Upload files

---
+++ Assets/Scripts/ControlaArma.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaArma : MonoBehaviour
+{
+    public GameObject Bala;
+    public GameObject CanoDaArma;
+    public AudioClip SomDoTiro;
+	
+	void Update ()
+    {
+		if(Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(Bala, CanoDaArma.transform.position, CanoDaArma.transform.rotation);
+            ControlaAudio.instancia.PlayOneShot(SomDoTiro);
+        }
+	}
+}
---
+++ Assets/Scripts/ControlaCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaCamera : MonoBehaviour {
+
+    public GameObject Jogador;
+    private Vector3 distCompensar;
+
+	void Start ()
+    {
+        distCompensar = transform.position - Jogador.transform.position;
+	}
+
+	void Update ()
+    {
+        transform.position = Jogador.transform.position + distCompensar;
+	}
+}
